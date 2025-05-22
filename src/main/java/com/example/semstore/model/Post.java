17ab134b580fc9a725e14a7dde7827366564e070
semzz <semzzcs@gmail.com>
2025-05-22 12:10:36 +0300

    fix: table name posts

--- src/main/java/com/example/semstore/model/Post.java
+++ src/main/java/com/example/semstore/model/Post.java
@@ -3,11 +3,13 @@ package com.example.semstore.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "posts")
 public class Post {
     @Id
     @GeneratedValue
